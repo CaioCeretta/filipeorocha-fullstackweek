a55bb8090012c3e229bf371c24192a6f65646413
andandtex <andandtex@gmail.com>
2024-08-05 11:23:44 -0300

    chore: add shadcn

--- fsw-barber/src/app/page.tsx
+++ fsw-barber/src/app/page.tsx
@@ -1,8 +1,11 @@
 "use client"
+
+import { Button } from "./_components/ui/button";
+
 //The use client makes a component PARTIALLY rendered on the server, while the server is fully
 
 export default function Home() {
   return (
-    <h1 className="py-4 text-red-500 bg-red-400  mb-5">Home</h1>
+    <Button>Test</Button>
   );
 }
