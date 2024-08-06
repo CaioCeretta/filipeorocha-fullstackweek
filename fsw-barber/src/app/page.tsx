9e5eb0ee49f2950fb6ae771e575b2daedde722c1
andandtex <andandtex@gmail.com>
2024-08-05 22:56:23 -0300

    Test

--- fsw-barber/src/app/page.tsx
+++ fsw-barber/src/app/page.tsx
@@ -6,10 +6,6 @@ import { Button } from './_components/ui/button'
 
 import React from 'react'
 
-const name = 'test'
-
-
-
 export default function Home() {
   return <Button className="mb-5 mt-10 flex text-red-500">Test</Button>
 }
