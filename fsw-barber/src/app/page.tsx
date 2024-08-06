35a6d756f403ec02b0a5c785f461f50a54548fc6
andandtex <andandtex@gmail.com>
2024-08-05 22:22:48 -0300

    test

--- fsw-barber/src/app/page.tsx
+++ fsw-barber/src/app/page.tsx
@@ -1,14 +1,15 @@
-"use client"
+'use client'
 
-//The use client makes a component PARTIALLY rendered on the server, while the server is fully
+// The use client makes a component PARTIALLY rendered on the server, while the server is fully
 
-import { Button } from "./_components/ui/button";
+import { Button } from './_components/ui/button'
 
 import React from 'react'
 
+const name = 'test'
+
+
 
 export default function Home() {
-  return (
-    <Button className="text-red-500 flex m-5">Test</Button>
-  );
+  return <Button className="mb-5 mt-10 flex text-red-500">Test</Button>
 }
