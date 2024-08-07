54a7c74f235c1743ca0969b1a3f9bcdf432532e4
andandtex <andandtex@gmail.com>
2024-08-06 22:50:35 -0300

    feat: add quick search

--- fsw-barber/src/app/page.tsx
+++ fsw-barber/src/app/page.tsx
@@ -4,21 +4,25 @@ import Header from './_components/header'
 
 import { Button } from './_components/ui/button'
 
-import React from 'react'
-import { Input } from './_components/ui/input'
-import { SearchIcon } from 'lucide-react'
+import { EyeIcon, FootprintsIcon, SearchIcon } from 'lucide-react'
 import Image from 'next/image'
-import { Card, CardContent } from './_components/ui/card'
-import { Badge } from './_components/ui/badge'
-import { Avatar, AvatarImage } from './_components/ui/avatar'
-import { db } from './_lib/prisma'
 import BarberShopItem from './_components/barbershop-item'
+import { Avatar, AvatarImage } from './_components/ui/avatar'
+import { Badge } from './_components/ui/badge'
+import { Card, CardContent } from './_components/ui/card'
+import { Input } from './_components/ui/input'
+import { db } from './_lib/prisma'
 
 export default async function Home() {
 
   const barbershops = await db.barbershop.findMany({})
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: 'desc'
+    }
+  })
 
-  console.log({barbershops})
+  console.log({ barbershops })
 
   return (
     <div>
@@ -30,12 +34,43 @@ export default async function Home() {
         <h2 className="text-xl">Hello, Caio</h2>
         <p>Monday, August 5th. 2024</p>
         {/* Search */}
-        <div className="mt-6 flex items-center gap-2">
+        <div className="mt-6 flex items-center gap-2 ">
           <Input placeholder="Search"></Input>
           <Button>
             <SearchIcon />
           </Button>
         </div>
+
+        {/* Quick Search */}
+        <div className="mt-6 flex items-center gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          <Button className='gap-2' variant={'secondary'}>
+            <Image src="/cabelo.svg" width={16} height={16} alt='Hair' />
+            Hair
+            </Button>
+
+          <Button className='gap-2' variant={'secondary'}>
+            <Image src="/barba.svg" width={16} height={16} alt='Beard' />
+            Beard
+          </Button>
+
+          <Button className='gap-2' variant={'secondary'}>
+            <Image src="/acabamento.svg" width={16} height={16} alt='Finish' />
+            Finishing
+          </Button>
+
+          <Button className='gap-2' variant={'secondary'}>
+            <FootprintsIcon className="w-4 h-4"  />
+            Outline
+          </Button>
+
+          <Button className='gap-2' variant={'secondary'}>
+            <EyeIcon className="w-4 h-4"/>
+            Eyebrows
+          </Button>
+
+        </div>
+
+
         {/* Image */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
@@ -73,10 +108,30 @@ export default async function Home() {
         <h2 className='mb-3 mt-6 text-xs font-bold uppercase text-gray-400'>Recommended</h2>
         <div className='flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden'>
           {barbershops.map(barbershop => (
-            <BarberShopItem key={barbershop.id} barbershop={barbershop}/>
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
+
+        <h2 className='mb-3 mt-6 text-xs font-bold uppercase text-gray-400'>Popular Barbershops</h2>
+        <div className='flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden'>
+          {popularBarbershops.map(barbershop => (
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+
+
       </div>
+
+      {/* Footer */}
+      <footer>
+        <Card>
+          <CardContent className='px-5 py-6'>
+            <p className="text-sm font-bold text-gray-400"> &copy; 2024 Copyright <span className="font-bold">FSW Barber</span></p>
+          </CardContent>
+        </Card>
+      </footer>
+
     </div>
+
   )
 }
