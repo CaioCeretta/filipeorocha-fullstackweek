514454e666b9f0db8b8aa105bb8934581a6ca8dc
andandtex <andandtex@gmail.com>
2024-08-11 22:20:48 -0300

    fix: fix getBookings fn

--- fsw-barber/src/app/_actions/get-bookings.ts
+++ fsw-barber/src/app/_actions/get-bookings.ts
@@ -1,3 +1,5 @@
+'use server'
+
 import { endOfDay, startOfDay } from "date-fns"
 import { db } from "../_lib/prisma"
 
@@ -7,7 +9,7 @@ interface GetBookingsProps {
 }
 
 
-export const getBookings = ({serviceId, date}: GetBookingsProps) => {
+export const getBookings = ({date}: GetBookingsProps) => {
 //In this case we can remove the async because it will return a promise
 //we will already await when we call this function, so it would be redundant
 
